1412e8132098ec5121069bac8c4341d0305979b0
HugoXR <rodrigo.x.viera.guesuh@gmail.com>
2022-04-27 07:45:11 -0300

    Tratando as unidades

--- Orbita.py
+++ Orbita.py
@@ -1,33 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+from astropy import constants
+from astropy import units
 
-G = 6.674184E-11 # m^3 kg^-1 s^-2
-M_1 = 1E30 # Star Mass 1 in Kg
-M_2 = 1E30 # Star Mass 2 in Kg
-M_p = 1E15 # Planet Mass in Kg
-
-X = np.zeros(18)
-
-X[0] = 1E11 # Position x of Star 1 in m
-X[1] = 2E11 # Position y of Star 1 in m
-X[2] = 0 # Position z of Star 1 in m
-X[3] = -1E11 # Position x of Star 2 in m
-X[4] = -2E11 # Position y of Star 2 in m
-X[5] = 0 # Position z of Star 2 in m
-X[6] = 10E11 # Position x of Planet in m
-X[7] = 0 # Position y of Planet in m
-X[8] = 0 # Position z of Planet in m
-
-X[9] = 2E3
-X[10] = 3E3
-X[11] = 0
-X[12] = 5E3
-X[13] = 3E3
-X[14] = 0
-X[15] = 2E3
-X[16] = 3E3
-X[17] = 0
 
 def dxdt(X, t):
     r12 = X[0:3] - X[3:6] # Position between Star 1 and 2
@@ -37,5 +13,34 @@ def dxdt(X, t):
     mod_rp1 = np.linalg.norm(rp1) # Module of rp1
     mod_rp2 = np.linalg.norm(rp2) # Module of rp2
     return np.hstack((np.array(X[9:]), -(G*M_2*r12)/(mod_r12**3),
-                      -(G*M_1*r12)/(mod_r12**3), ((-(G*M_1*rp1)/(mod_rp1**3)) +
+                      (G*M_1*r12)/(mod_r12**3), ((-(G*M_1*rp1)/(mod_rp1**3)) +
                      (-(G*M_2*rp2)/(mod_rp2**3)))))
+
+
+M_1 = (1*units.Msun).decompose().value # Massa da primeira estrela = 1 Massa Solar
+M_2 = (5*units.Msun).decompose().value # Massa da segunda estrela = 5 Massas Solares
+M_P = (10*units.Mjup).decompose().value # Massa do planeta = 10 Massas de Jupiter
+
+mu = (M_1 * M_2)/(M_1 + M_2) # Massa reduzida das duas estrelas
+G = constants.G.value # Constante da Gravitacao
+
+a_S = (1 * units.AU).decompose().value # Distancia inicial das estrela (semi-eixo maior) = 1 Unidade Astronomica
+a_P = (2 * units.AU).decompose().value # Distancia inicial do planeta (semi-eixo maior) = 1 Unidade Astronomica
+
+r_1 = a_S*(mu/M_1) # Posicao relativa da primeira estrela
+r_2 = -a_S*(mu/M_2) # Posicao relativa da segunda estrelas
+r_p = a_P # Posicao do planeta
+
+P = 2*np.pi*np.sqrt((a_S**3)/(G*(M_1 + M_2))) # Periodo da orbita das estrelas (entre si) pela terceira lei de kepler
+P_p = 2*np.pi*np.sqrt((a_P**3)/(G*(M_1 + M_2))) # Periodo da orbita do planeta em torno das estrelas pela terceira lei de kepler
+
+v_1 = 2*np.pi*r_1/P # Velocidade inicial da estrela 1
+v_2 = 2*np.pi*r_2/P # Velocidade inicial da estrela 2
+v_p = 2*np.pi*r_p/P_p # Velocidade inicial do planeta
+
+X = np.array([r_1, 0, 0, r_2, 0, 0, r_p, 0, 0, v_1, 0, 0, v_2, 0, 0, v_p, 0, 0])
+t_orbit = np.linspace(0, P_p, 5)
+
+trajetoria = odeint(dxdt, X, t_orbit)
+
+
